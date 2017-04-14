f9234fe2089a181c2660a7aea780118f31bd72bc
vinilopes03 <vinilopes03@gmail.com>
2017-04-14 03:15:48 -0300

    Add
    add

---
+++ src/main/webapp/exemplo-javascript.js
@@ -0,0 +1,34 @@
+// JavaScript source code
+
+function calcular() {
+
+    var high = document.formulario.altura.value.replace(",",".");
+    var peso = document.formulario.peso.value.replace(",",".");
+    var op = document.formulario.sex.value;
+
+    var imc = peso / (Math.pow(high, 2));
+
+    if (op == "one") {
+        if (imc < 20.00) {
+            alert("IMC: " + imc.toFixed(2) + "\nAbaixo do peso");
+        }
+        else if (imc > 26.4) {
+            alert("IMC: " + imc.toFixed(2) + "\nAcima do peso");
+        }
+        else {
+            alert("IMC: " + imc.toFixed(2) + "\nPeso normal");
+        }
+
+    }
+    else if (op =="two") {
+        if (imc < 19.1) {
+            alert("IMC: " + imc.toFixed(2) + "\nAbaixo do Peso");
+        }
+        else if (imc > 25.8) {
+            alert("IMC: " + imc.toFixed(2) + "\nAcima do peso");
+        }
+        else {
+            alert("IMC: " + imc.toFixed(2) + "\nPeso normal");
+        }
+    }
+}
